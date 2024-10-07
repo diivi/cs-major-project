5d0508b57b98e199167c26f58251ebb9dce775bf
Divyansh Singh <divyanshanandsingh@gmail.com>
2024-10-07 18:29:26 +0530

    chat and rag

---
+++ app.py
@@ -0,0 +1,139 @@
+from openai import AsyncOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+import os
+import chainlit as cl
+import faiss
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+import PyPDF2
+
+client = AsyncOpenAI()
+cl.instrument_openai()
+settings = {
+    "model": "gpt-4o",
+    "temperature": 0.7,
+    "max_tokens": 500,
+    "top_p": 1,
+    "frequency_penalty": 0,
+    "presence_penalty": 0,
+}
+
+from dotenv import load_dotenv
+load_dotenv()
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+index = faiss.IndexFlatL2(len(embeddings.embed_query("hello world")))
+vector_store = FAISS(
+    embedding_function=embeddings,
+    index=index,
+    docstore=InMemoryDocstore(),
+    index_to_docstore_id={},
+)
+
+def format_docs(docs):
+  return "\n\n".join(doc.page_content for doc in docs)
+
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    if (username, password) == ("admin", "admin"):
+        return cl.User(
+            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
+        )
+    else:
+        return None
+
+@cl.set_starters
+async def set_starters():
+    return [
+        cl.Starter(
+            label="Explain a privacy policy",
+            message="Explain the privacy policy of facebook.com to me in simple terms. I'm not sure what it means.",
+            icon="/public/contract.svg",
+            ),
+
+        cl.Starter(
+            label="Be my lawyer",
+            message="I need help writing a contract for a freelance project. Can you help me draft one?",
+            icon="/public/law.svg",
+            ),
+        cl.Starter(
+            label="Python script for daily email reports",
+            message="Write a script to automate sending daily email reports in Python, and walk me through how I would set it up.",
+            icon="/public/terminal.svg",
+            ),
+        cl.Starter(
+            label="Text inviting friend to wedding",
+            message="Write a text asking a friend to be my plus-one at a wedding next month. I want to keep it super short and casual, and offer an out.",
+            icon="/public/write.svg",
+            )
+        ]
+
+client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+@cl.on_chat_start
+def start_chat():
+    cl.user_session.set(
+        "message_history",
+        [{"role": "system", "content": "You are a helpful legal assistant for lawyers and civilians from India."}],
+    )
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+@cl.on_message
+async def main(message: cl.Message):
+    if message.elements:
+        pdf = [file for file in message.elements if "pdf" in file.mime][0]
+        pdf = PyPDF2.PdfReader(pdf.path)
+        pdf_text = ""
+        for page in pdf.pages:
+            pdf_text += page.extract_text()
+        docs = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100, length_function=len, is_separator_regex=False).create_documents([pdf_text])
+
+        metadatas = [{"source": f"{i}-pl"} for i in range(len(docs))]
+        for doc in docs:
+            doc.metadata = metadatas.pop(0)
+        vector_store.add_documents(documents=docs)
+        vector_store.save_local("faiss_index_constitution")
+
+        retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 6})
+        prompt = hub.pull("rlm/rag-prompt")
+        llm = ChatOpenAI(model="gpt-4o-mini")
+
+        rag_chain = (
+            {"context": retriever | format_docs, "question": RunnablePassthrough()}
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
+
+        response = rag_chain.invoke(message.content)
+
+        message = cl.Message(content=response)
+
+        await message.send()
+
+        return
+    
+    message_history = cl.user_session.get("message_history")
+    message_history.append({"role": "user", "content": message.content})
+
+    msg = cl.Message(content="")
+
+    stream = await client.chat.completions.create(
+        messages=message_history, stream=True, **settings
+    )
+
+    async for part in stream:
+        if token := part.choices[0].delta.content or "":
+            await msg.stream_token(token)
+
+    message_history.append({"role": "assistant", "content": msg.content})
+    await msg.update()
+
